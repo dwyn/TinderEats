c21c32f33ab1d1b0e5bd9e9add128d28ede6f21e
mdacht777 <mdacht777@gmail.com>
2020-10-29 17:56:15 -0600

    updated user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :restaurants
+  has_many :cuisines, through: :restaurants
+  has_many :reviews
+  has_many :restaurants, through: :reviews
+  
 end
