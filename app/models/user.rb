da4fd7aa7fb99d8cf61c2b07eae6a3c1eb982e5c
dwieb <dwayne.harmon@gmail.com>
2020-10-29 18:28:33 -0500

    Updated User Model... and stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-	has_many :restaurants
-	has_many :reviews
-	has_many :cuisines, through: :restaurants  
-	has_many :restaurants, through: :reviews	
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
